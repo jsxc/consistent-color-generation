a07cec54b8c42ab5fc94602e4a2ec5fc019a3b34
sualko <klaus@jsxc.org>
2017-12-22 12:23:44 +0100

    fix test cases

--- test/index.js
+++ test/index.js
@@ -6,30 +6,33 @@ var testVectors = {
       'Romeo': [0.281,0.790,1.000],
       'juliet@capulet.lit': [0.337,1.000,0.000],
       '😺': [0.347,0.756,1.000],
+      'council': [0.732,0.560,1.000],
    },
    redGreenCorrection: {
-      'Romeo': [0.938,0.799,0.000],
-      'juliet@capulet.lit': [1.000,0.420,0.499],
-      '😺': [0.912,0.812,0.000],
+      'Romeo': [1.000,0.674,0.000],
+      'juliet@capulet.lit': [1.000,0.359,1.000],
+      '😺': [1.000,0.708,0.000],
+      'council': [0.732,0.904,0.000],
    },
    blueCorrection: {
-      'Romeo': [0.031,1.000,0.472],
-      'juliet@capulet.lit': [0.548,0.998,0.000],
-      '😺': [0.031,1.000,0.505],
+      'Romeo': [1.000,0.674,0.000],
+      'juliet@capulet.lit': [0.337,1.000,0.000],
+      '😺': [1.000,0.708,0.000],
+      'council': [0.732,0.904,0.000],
    }
 }
 
 function runTestCasesForRGBGeneration(testVectors, correction) {
    for (text in testVectors) {
-      var expectedColor = testVectors[text];
+      ((text, correction, expectedColor) => {
+         it(text, function(){
+            var color = getRGB(text, correction);
 
-      it(text, function(){
-         var color = getRGB(text, correction);
-
-         expect(color.r).to.be.closeTo(expectedColor[0], 0.01);
-         expect(color.g).to.be.closeTo(expectedColor[1], 0.01);
-         expect(color.b).to.be.closeTo(expectedColor[2], 0.01);
-      });
+            expect(color.r, 'Red value').to.be.closeTo(expectedColor[0], 0.01);
+            expect(color.g, 'Green value').to.be.closeTo(expectedColor[1], 0.01);
+            expect(color.b, 'Blue value').to.be.closeTo(expectedColor[2], 0.01);
+         });}
+      )(text, correction, testVectors[text]);
    }
 }
 
